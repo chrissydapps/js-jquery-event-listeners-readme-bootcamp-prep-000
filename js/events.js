f8d4dd5cdc868c6ec6dbf86172094ba99f7eeb72
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-14 17:35:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,6 +23,11 @@ function init() {
   }
 }
 <<<<<<< HEAD
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
 function frameIt() {
   $('img').on('load', function() {
     $('img').addClass('tasty')
